794431da03a4d49e33208fa36d18d7f48f4a97a4
Sergey Kucheryavskiy <svkucheryavski@gmail.com>
2021-10-01 20:41:19 +0200

    added building step for each app

--- share.py
+++ share.py
@@ -1,30 +1,42 @@
 from shutil import copyfile, move, rmtree
-from os import makedirs, path
+import subprocess
 import json
+import os
 
 SRC_DIR = "./apps/"
-DST_DIR = "../graasta.com/public/apps"
+DST_DIR = "../graasta.web.static/public/apps"
 
 # which blocks and apps to process
 app_blocks = [
    {
       "title": "Descriptive statistics and plots",
-      "apps": ["asta-b101", "asta-b102"]
+      "apps": ["asta-b101", "asta-b102", "asta-b103"]
    }
 ]
 
 # copy application files and create JSON with meta data
 out = []
+wd = os.getcwd()
+print(wd)
 for block in app_blocks:
 
     apps = [];
     for app in block["apps"]:
         print(SRC_DIR + app)
-        makedirs(path.join(SRC_DIR, app), exist_ok = True)
+
+        # build the app
+        os.chdir(app)
+        subprocess.call("npm run build", shell=True)
+        os.chdir(wd)
+
+        # copy the files
+        os.makedirs(os.path.join(SRC_DIR, app), exist_ok = True)
         copyfile(app + "/public/" + app + ".css", SRC_DIR + app + "/" + app + ".css")
         copyfile(app + "/public/" + app + ".js", SRC_DIR + app + "/" + app + ".js")
         copyfile(app + "/public/" + app + ".js.map", SRC_DIR + app + "/" + app + ".js.map")
         copyfile(app + "/public/" + "index.html", SRC_DIR + app + "/index.html")
+
+        # add app info into json
         app_info = json.load(open(app + "/info.json"))
         apps.append(app_info)
 
@@ -40,6 +52,6 @@ with open(SRC_DIR + "apps.js", 'w') as outfile:
     outfile.write("const appBlocks = " + json.dumps(out) + "; export default appBlocks;")
 
 # move everything to graasta.com repository
-if path.exists(DST_DIR):
+if os.path.exists(DST_DIR):
     rmtree(DST_DIR)
 move(SRC_DIR, DST_DIR)
