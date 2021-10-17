b139d124e49b15e3ac37c04c1a82b6882e4b8984
Sergey Kucheryavskiy <svkucheryavski@gmail.com>
2021-10-17 16:58:09 +0200

    added app-b202

---
+++ asta-b202/src/App.svelte
@@ -0,0 +1,128 @@
+<script>
+   import {seq, subset, rep, shuffle} from 'stat-js';
+
+   // children blocks
+   import PopulationPlot from '../../asta-b201/src/PopulationPlot.svelte';
+   import SamplePlot from '../../asta-b201/src/SamplePlot.svelte';
+   import CIPlot from './CIPlot.svelte';
+
+   // common blocks
+   import {default as StatApp} from '../../shared/StatApp.svelte';
+   import AppControlArea from '../../shared/AppControlArea.svelte';
+   import AppControlButton from '../../shared/AppControlButton.svelte';
+   import AppControlSwitch from '../../shared/AppControlSwitch.svelte';
+   import AppControlRange from '../../shared/AppControlRange.svelte';
+
+   // size of population and vector with element indices
+   const popSize = 400;
+   const popIndex = seq(1, popSize, popSize);
+   const colors = ["#ff0000", "#0000ff"];
+
+   // variable parameters
+   let popProp = 0.50;
+   let sampSize = 5;
+
+   function takeNewSample() {
+      sample = subset(shuffle(popIndex), seq(1, sampSize, sampSize));
+   }
+
+   // generate groups of population randomly
+   $: groups = shuffle(rep(1, Math.round(popProp * popSize)).concat(rep(2, Math.round((1 - popProp) * popSize))));
+
+   // take a sample if population proportion has changed
+   $: sample = popProp ? subset(shuffle(popIndex), seq(1, sampSize, sampSize)) : NULL;
+</script>
+
+<StatApp>
+   <div class="app-layout">
+
+      <!-- plot for population individuals  -->
+      <div class="app-population-plot-area">
+         <PopulationPlot {groups} {sample} {colors} />
+      </div>
+
+      <!-- plot for sample individuals -->
+      <div class="app-sample-plot-area">
+         <SamplePlot {groups} {sample} {colors} />
+      </div>
+
+      <!-- confidence intervals and statistic table -->
+      <div class="app-ci-plot-area">
+         <CIPlot {groups} {sample} {colors} />
+      </div>
+
+      <!-- control elements -->
+      <div class="app-controls-area">
+         <AppControlArea>
+            <AppControlRange id="popProp" label="Proportion" bind:value={popProp} min={0.1} max={0.9} step={0.01} decNum={2} />
+            <AppControlSwitch id="sampleSize" label="Sample size" bind:value={sampSize} options={[5, 10, 20, 40]} />
+            <AppControlButton id="newSample" label="Sample" text="Take new" on:click={takeNewSample} />
+         </AppControlArea>
+      </div>
+
+   </div>
+
+   <div slot="help">
+      <h2>Sample based confidence interval for proportion</h2>
+      <p>
+         This app is similar to <code>asta-b201</code>, but, in this case, confidence interval is computed
+         based on sample proportion. This requires larger sample size, so for every category you need at
+         least 10 individuals in your sample. For example, if proportion is 20%, you need sample size of at
+         least n = 50 to make a reliable interval (20% of 50 is 10). For p = 10% the sample size should
+         be n = 100.
+      </p>
+      <p>
+         The app shows 95% confidence interval computed for current sample as a plot on the right side. So,
+         every time you take a new sample, this also results in a new confidence interval. The vertical red line
+         on this plot shows the population proportion, which in real life we do not know.
+      </p>
+   </div>
+</StatApp>
+
+<style>
+
+.app-layout {
+   width: 100%;
+   height: 100%;
+   position: relative;
+   display: grid;
+   grid-template-areas:
+      "pop sampplot"
+      "pop ciplot"
+      "pop controls";
+   grid-template-rows: 150px 1fr 1fr;
+   grid-template-columns: 65% 35%;
+}
+
+
+.app-population-plot-area {
+   grid-area: pop;
+   box-sizing: border-box;
+   height: 100%;
+   width: 100%;
+   padding-right: 20px;
+}
+
+
+.app-sample-plot-area {
+   grid-area: sampplot;
+   background: black;
+}
+
+.app-sample-plot-area :global(.plot) {
+   min-height: 150px;
+}
+
+.app-sample-plot-area {
+   height: 150px;
+}
+
+.app-ci-plot-area {
+   grid-area: ciplot;
+}
+
+.app-controls-area {
+   grid-area: controls;
+}
+
+</style>
---
+++ asta-b202/src/CIPlot.svelte
@@ -0,0 +1,102 @@
+<script>
+   import {sum, seq, subset, min, max, dnorm} from 'stat-js';
+   import {Axes, XAxis, LineSeries, AreaSeries, Segments} from 'svelte-plots-basic';
+   import {DataTable} from 'svelte-plots-stat';
+
+   export let groups;
+   export let sample;
+   export let colors;
+
+   let sampStat = [];
+   let popProp;
+   let sampProp;
+   let sampleSize;
+   let sampleSizeOld = sample.length;
+
+   // when sample size has changed - reset statistics
+   $: {
+      sampleSize = sample.length;
+      if (sampleSize != sampleSizeOld) {
+         sampleSizeOld = sampleSize;
+         sampStat = [];
+      }
+   }
+
+   // proportion of current sample
+   $: sampProp = 1 - sum(subset(groups, sample).map(v => v - 1)) / sampleSize;
+
+   // if population has changed - reset statistics
+   $: {
+      popProp = 1 - sum(groups.map(v => v - 1)) / groups.length;
+      sampStat = [];
+   }
+
+   // standard error for CI
+   $: sd = Math.sqrt((1 - sampProp) * sampProp / sampleSize);
+
+   // PDF curve
+   $: x = seq(sampProp - 3.5 * sd, sampProp + 3.5 * sd, 100);
+   $: f = dnorm(x, sampProp, sd);
+
+   // CI and CI area
+   $: ci = [max([0, sampProp - 1.96 * sd]), min([1, sampProp + 1.96 * sd])];
+   $: cix = seq(ci[0], ci[1], 100);
+   $: cif = dnorm(cix, sampProp, sd);
+
+   // if new sample is taken, add true if it is inside CI and false otherwise
+   $: sampStat = [...sampStat, popProp >= ci[0] && popProp <= ci[1] ? 1 : 0];
+
+   // numeric values for stat table
+   $: nSamples = sampStat.length;
+   $: nSamplesInside = sum(sampStat);
+
+   // text values for stat table
+   $: tableSampProp = sampProp.toFixed(2)
+   $: tableCI = `[${ci[0].toFixed(2)}, ${ci[1].toFixed(2)}]`;
+   $: tableNSamplesInside = `${nSamplesInside} (${(nSamplesInside/nSamples * 100).toFixed(1)}%)`;
+</script>
+
+<div class="ci-plot">
+   <!-- plot with population based CI and position of current sample proportion -->
+   <Axes limX={[-0.02, 1.02]} limY={[-0.01, max(f) * 1.1]}>
+      <AreaSeries xValues={cix} yValues={cif} lineColor={colors[0] + "40"} fillColor={colors[0] + "40"}/>
+      <LineSeries xValues={x} yValues={f} lineColor={colors[0] + "40"} />
+      <Segments xStart={[popProp]} xEnd={[popProp]} yStart={[0]} yEnd={[max(f) * 1.1]} lineColor={colors[0]} />
+      <XAxis slot="xaxis" ></XAxis>
+   </Axes>
+</div>
+
+<div class="stat-table">
+   <!-- plot with statistics -->
+   <DataTable
+      variables={[
+         {label: "Confidence interval (CI)", values: [tableCI]},
+         {label: "Proportion of current sample", values: [tableSampProp]},
+         {label: "# of samples taken", values: [nSamples]},
+         {label: "# of samples inside CI", values: [tableNSamplesInside]},
+      ]}
+      decNum={[0, 0]}
+      horizontal={true}
+   />
+</div>
+
+<style>
+   .stat-table {
+      padding: 1em;
+   }
+
+   .stat-table :global(.datatable) {
+      font-size: 0.85em;
+      color: #606060;
+   }
+
+   .stat-table :global(.datatable__label) {
+      font-weight: normal;
+   }
+
+   .stat-table :global(.datatable__value) {
+      padding-left: 1em;
+      font-weight: bold;
+      color: #505050;
+   }
+</style>
