90303d909c1485ccda1fa4575f358b7bad3fb7a7
Sergey Kucheryavskiy <svkucheryavski@gmail.com>
2021-11-09 13:11:13 +0100

    asta-b201: PopulationPlot - made population size text in plot title dynamic

--- asta-b201/src/PopulationPlot.svelte
+++ asta-b201/src/PopulationPlot.svelte
@@ -39,12 +39,12 @@
    $: samp2Y = subset(pop[1], samp2Index);
 </script>
 
-<Axes title="Population (N = 400)" limX={mrange(pop[0], 0.1)} limY={mrange(pop[1], 0.05)} >
+<Axes title="Population (N = {popSize})" limX={mrange(pop[0], 0.1)} limY={mrange(pop[1], 0.05)} >
    <!-- population points  -->
-   <ScatterSeries xValues={pop1X} yValues={pop1Y} borderWidth={2} markerSize={2} borderColor={colors[0] + "50"} faceColor={colors[0] + "50"}/>
-   <ScatterSeries xValues={pop2X} yValues={pop2Y} borderWidth={2} markerSize={2} borderColor={colors[1] + "50"} faceColor={colors[1] + "50"}/>
+   <ScatterSeries xValues={pop1X} yValues={pop1Y} borderWidth={1.5} markerSize={1.5} borderColor={colors[0] + "50"} faceColor={colors[0] + "50"}/>
+   <ScatterSeries xValues={pop2X} yValues={pop2Y} borderWidth={1.5} markerSize={1.5} borderColor={colors[1] + "50"} faceColor={colors[1] + "50"}/>
    <!-- sample points on top  -->
-   <ScatterSeries xValues={samp1X} yValues={samp1Y} borderWidth={4} markerSize={2.15} borderColor={colors[0]} faceColor={"white"}/>
-   <ScatterSeries xValues={samp2X} yValues={samp2Y} borderWidth={4} markerSize={2.15} borderColor={colors[1]} faceColor={"white"}/>
+   <ScatterSeries xValues={samp1X} yValues={samp1Y} borderWidth={2.75} markerSize={1.65} borderColor={colors[0]} faceColor={"white"}/>
+   <ScatterSeries xValues={samp2X} yValues={samp2Y} borderWidth={2.75} markerSize={1.65} borderColor={colors[1]} faceColor={"white"}/>
 </Axes>
 
