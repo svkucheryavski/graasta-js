5611736affadaa5a2a7c8001a93eaf17d1d01a64
Sergey Kucheryavskiy <svkucheryavski@gmail.com>
2021-11-29 20:53:03 +0100

    added common js with color object and helper functions

---
+++ shared/graasta.js
@@ -0,0 +1,25 @@
+export let colors = {
+   plots: {
+      // population colors
+      POPULATIONS: ["#33668850", "#ff990050"],
+      SAMPLES: ["#336688", "#ff9900"],
+
+      // statistics on plot legend
+      STAT_NAME: "#808080",
+      STAT_VALUE: "#202020"
+   }
+};
+
+
+export function formatLabels(labels) {
+
+   if (!Array.isArray(labels)) labels = [labels];
+   let labelsStr = Array(length = labels.length);
+
+
+   for (let i = 0; i < labels.length; i++) {
+      labelsStr[i] =    "<tspan fill=" + colors.plots.STAT_NAME + ">" + labels[i].name + ":</tspan> " + labels[i].value;
+   }
+
+   return labelsStr;
+}
