936b9eb14e99b3821767c95a8e56c5e1abf19f88
Sergey Kucheryavskiy <svkucheryavski@gmail.com>
2021-12-12 17:29:45 +0100

    refactoring and small imrovements

--- shared/plots/TTestPlot.svelte
+++ shared/plots/TTestPlot.svelte
@@ -1,6 +1,6 @@
 <script>
-   import {seq, max, mrange, dt} from 'stat-js';
-   import TestPlot from '../../shared/plots/TestPlot.svelte';
+   import {seq, dt} from "stat-js";
+   import TestPlot from "../../shared/plots/TestPlot.svelte";
 
    export let testRes;
    export let showLegend = true;
@@ -8,18 +8,24 @@
    export let xLabel = "";
    export let limX = undefined;
    export let limY = undefined;
-   export let mainColor = "#404040";
+   export let mainColor = "#808080";
    export let reset;
    export let clicked;
+
+   const signs = {"both": "=", "left": "≥", "right": "≤"};
 
    // PDF curve for sampling distribution
    $: t = seq(-10, 10, 300)
    $: x = t.map(v => v * testRes.se + testRes.effectExpected);
    $: f = dt(t, testRes.DoF);
 
-   // critical t-value
-   $: crit = testRes.tail === "both" ?
-      [-Math.abs(testRes.effectObserved), Math.abs(testRes.effectObserved)] : [testRes.effectObserved];
+   // critical values
+   $: dp = Math.abs(testRes.effectObserved - testRes.effectExpected)
+   $: crit = testRes.tail === "both" ? [testRes.effectExpected - dp, testRes.effectExpected + dp] : [testRes.effectObserved];
+
+   $: H0LegendStr = testRes.test === "Two sample t-test" ?
+      `H0: µ1 – µ2 = 0` :
+      `H0: µ ${signs[testRes.tail]} ${testRes.effectExpected.toFixed(2)}`;
 </script>
 
 <TestPlot
