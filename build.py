06a3dddcc4ff0ccd3194961ad702f7dc7a344ae0
Sergey Kucheryavskiy <svkucheryavski@gmail.com>
2022-06-29 15:38:06 +0200

    added asta-b307 to build list

--- build.py
+++ build.py
@@ -30,7 +30,7 @@ app_blocks = [
    },
    {
       "title": "Relationship between two variables",
-      "apps": ["asta-b301", "asta-b302", "asta-b303", "asta-b304", "asta-b305", "asta-b306"]
+      "apps": ["asta-b301", "asta-b302", "asta-b303", "asta-b304", "asta-b305", "asta-b306", "asta-b307"]
    }
 ]
 
