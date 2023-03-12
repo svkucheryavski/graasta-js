346e255f9a463d19b1253175213c7be10a6957f6
Sergey Kucheryavskiy <svkucheryavski@gmail.com>
2023-03-12 20:38:15 +0100

    improvements to shared components

--- shared/plots/TTestPlot.svelte
+++ shared/plots/TTestPlot.svelte
@@ -1,29 +1,30 @@
 <script>
-   import { seq, dt } from "mdatools/stat";
-   import TestPlot from "../../shared/plots/TestPlot.svelte";
+   import { Vector } from 'mdatools/arrays';
+   import { dt } from 'mdatools/distributions';
+   import TestPlot from '../../shared/plots/TestPlot.svelte';
 
    export let testRes;
    export let showLegend = true;
-   export let H0LegendStr = "";
-   export let xLabel = "";
+   export let H0LegendStr = '';
+   export let xLabel = '';
    export let limX = undefined;
    export let limY = undefined;
-   export let mainColor = "#808080";
+   export let mainColor = '#808080';
    export let reset;
    export let clicked;
 
-   const signs = {"both": "=", "left": "≥", "right": "≤"};
+   const signs = {'both': '=', 'left': '≥', 'right': '≤'};
 
    // PDF curve for sampling distribution
-   $: t = seq(-10, 10, 300)
-   $: x = t.map(v => v * testRes.se + testRes.effectExpected);
+   $: t = Vector.seq(-10, 10, 0.02)
+   $: x = t.apply(v => v * testRes.se + testRes.effectExpected);
    $: f = dt(t, testRes.DoF);
 
    // critical values
    $: dp = Math.abs(testRes.effectObserved - testRes.effectExpected)
-   $: crit = testRes.tail === "both" ? [testRes.effectExpected - dp, testRes.effectExpected + dp] : [testRes.effectObserved];
+   $: crit = testRes.tail === 'both' ? [testRes.effectExpected - dp, testRes.effectExpected + dp] : [testRes.effectObserved];
 
-   $: H0LegendStr = testRes.test === "Two sample t-test" ?
+   $: H0LegendStr = testRes.test === 'Two sample t-test' ?
       `H0: µ1 – µ2 = 0` :
       `H0: µ ${signs[testRes.tail]} ${testRes.effectExpected.toFixed(2)}`;
 </script>
