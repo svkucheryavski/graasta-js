d23c5bd9852068097702d98eba1d9d8e8537a89f
Sergey Kucheryavskiy <svkucheryavski@gmail.com>
2023-03-14 09:46:30 +0100

    improvements to shared components

--- shared/plots/ANOVABoxplot.svelte
+++ shared/plots/ANOVABoxplot.svelte
@@ -1,14 +1,13 @@
 <script>
-   import { rep } from "mdatools/stat";
-   import { Axes, YAxis, Segments, ScatterSeries } from "svelte-plots-basic";
-
-   import BoxAndWhiskers from "./BoxAndWhiskers.svelte";
+   import { Vector } from 'mdatools/arrays';
+   import { Axes, YAxis, Segments, Points } from 'svelte-plots-basic/2d';
+   import { BoxAndWhiskers } from 'mdatools-plots/stat';
 
    export let popMeans;
    export let popSigma;
    export let samples;
-   export let color = "#202020";
-   export let boxColor = "#d0d0d0";
+   export let color = '#202020';
+   export let boxColor = '#d0d0d0';
    export let limY = [-75, 75];
    export let limX = [-0.5, 2.5];
 
@@ -16,7 +15,7 @@
    $: popRanges = popQuartiles.map(v => [v[0] - 1.5 * (v[2] - v[0]), v[2] + 1.5 * (v[2] - v[0])]);
 </script>
 
-<Axes {limX} {limY} >
+<Axes {limX} {limY} margins={[0.5, 1, 0.5, 0.2]}>
    {#each popQuartiles as p, i}
       <BoxAndWhiskers
          lineWidth={2}
@@ -24,12 +23,14 @@
          borderColor={boxColor}
          range={popRanges[i]}
          quartiles={popQuartiles[i]}
+         outliers={[]}
          boxPosition={i}
          boxSize={0.5}
          horizontal={false}
       />
       <Segments lineType={2} xStart={[-0.5]} xEnd={[2.5]} yStart={[0]} yEnd={[0]} lineColor="#909090" />
-      <ScatterSeries borderWidth={2}  faceColor="transparent" borderColor={color} markerSize={1.25} xValues={rep(i, samples[i].length)} yValues={samples[i]} />
+      <Points borderWidth={2}  faceColor="transparent" borderColor={color} markerSize={1.25}
+         xValues={Vector.fill(i, samples[i].length)} yValues={samples[i]} />
    {/each}
    <YAxis slot="yaxis" />
 </Axes>
--- shared/plots/ANOVATestPlot.svelte
+++ shared/plots/ANOVATestPlot.svelte
@@ -1,42 +1,45 @@
 <script>
-   import { seq, df, pf, sum, mean, max } from "mdatools/stat";
-   import { TextLegend } from "svelte-plots-basic";
+   import { Vector } from 'mdatools/arrays';
+   import { df, pf } from 'mdatools/distributions';
+   import { sum, mean, max } from 'mdatools/stat';
+   import { TextLegend } from 'svelte-plots-basic/2d';
 
    // shared components
-   import { formatLabels } from "../graasta.js";
+   import { formatLabels } from '../graasta.js';
 
    // shared components - plots
-   import TestPlot from "./TestPlot.svelte";
+   import TestPlot from './TestPlot.svelte';
 
-   export let mainColor = "#a0a0a0";
+   export let mainColor = '#a0a0a0';
    export let sysSample;
    export let errSample;
    export let reset;
    export let clicked;
 
-   const H0LegendStr = "H0: µA = µB = µC";
+   const H0LegendStr = 'H0: µA = µB = µC';
 
    $: DoFSys = sysSample.length - 1;
    $: DoFErr = sum(sysSample.map(v => v.length)) - DoFSys - 1;
-   $: grandMean = mean(sysSample.map(v => v[0]));
+   $: grandMean = mean(sysSample.map(v => v.v[0]));
 
-   $: MSSys = sum(sysSample.map(v => sum(v.map(x => (x - grandMean)**2)))) / DoFSys;
-   $: MSErr = sum(errSample.map(v => sum(v.map(x => x**2)))) / DoFErr;
+   $: MSSys = sum(sysSample.map(v => sum(v.apply(x => (x - grandMean)**2)))) / DoFSys;
+   $: MSErr = sum(errSample.map(v => sum(v.apply(x => x**2)))) / DoFErr;
 
    // compute sample statistics
    $: crit = MSSys/MSErr;
 
    // PDF curve for sampling distribution
-   $: x = seq(0.01, 10, 200);
+   $: x = Vector.seq(0.01, 10, 0.01);
    $: f = df(x, DoFSys, DoFErr);
 
    // compute p-value and x/y coordinates for corresponding area under PDF
    $: pValue = 1 - pf(crit, DoFSys, DoFErr);
-
+   $: console.log(sysSample.map(v => sum(v.apply(x => (x - grandMean)**2))))
+   $: console.log(MSErr)
 </script>
 
 <TestPlot limX={[-0.2, 10]} {x} {f} {pValue} {reset} {clicked} {H0LegendStr} {mainColor} crit={[crit]} alpha={0.05} tail="right">
    <!-- add legend item with current F-value -->
-   <TextLegend textSize={1.15} left={-0.2} top={max(f) * 1.025} dx="0.5em"
+   <TextLegend textSize={1.15} left={4} top={max(f) * 0.5} dx="0"
       elements={formatLabels([{name: "F-value", value: crit.toFixed(1)}])} />
 </TestPlot>
